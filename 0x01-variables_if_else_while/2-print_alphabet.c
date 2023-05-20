d861c4f3e6022ef78a1403e71b828fe61a126026
Hikmat <hikmatyusuf2018@gmail.com>
2023-05-19 17:00:16 -0700

    alphabeth in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int ip;
+
+	for (ip = 'a'; ip <= 'z'; i++)
+	{
+	putchar(ip);
+	}
+	putchar('\n');
+	return (0);
+}
