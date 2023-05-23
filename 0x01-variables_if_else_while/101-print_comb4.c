945436cb7f45ae432fd5546cc9c6646eda3b9f86
Hikmat <hikmatyusuf2018@gmail.com>
2023-05-22 20:08:48 -0700

    3 combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int p;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			for(p = 50; p <= 58; p++)
+			{
+				if (p > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(p);
+					if (i != 56 || j != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
