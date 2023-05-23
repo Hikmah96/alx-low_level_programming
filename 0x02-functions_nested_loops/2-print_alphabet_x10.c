790c8bf06e4569efdd81f920fc2b0a1d28400ca2
Hikmat <hikmatyusuf2018@gmail.com>
2023-05-23 11:15:42 -0700

    alphabeth ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that print
+ * alphabet ten times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int p
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (p = 0; p <= 10; p++)
+		{
+			if (p <= 10)
+				_putchar(i);
+		}
+		_puthchar('\n');
+	}
+}
