e1a4f809f31c650178e5f7c88621281ad1deac32
Hikmat <hikmatyusuf2018@gmail.com>
2023-06-11 10:11:28 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: always zero
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
