cfa4e5456fc04ccd0bc6609e668aa4a24e7aac7c
Hikmat <hikmatyusuf2018@gmail.com>
2023-06-11 10:02:56 -0700

    name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: always zero
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
