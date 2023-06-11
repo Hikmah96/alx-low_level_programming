246945065fa10d52f370bcf870731f3d4e06805d
Hikmat <hikmatyusuf2018@gmail.com>
2023-06-11 10:57:55 -0700

    argc argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
 
 /**
  * main - main function
@@ -11,6 +13,6 @@ int main(int argc, char *argv[] __attribute__((unused)))
 	int i;
 
 	for (i = 1; i < argc; i++)
-		printf("%d\n", i);
+		printf("%s\n", argv[i]);
 	return (0);
 }
