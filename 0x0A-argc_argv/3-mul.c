3590d6b807ec8af6b1c328e639f6cddae696ad91
Hikmat <hikmatyusuf2018@gmail.com>
2023-06-12 16:42:19 -0700

    argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,27 +2,24 @@
 #include <stdlib.h>
 
 /**
- * main - main function
+ * main - the main function
  * @argc: arguement count
  * @argv: arguement vector
+ * Return: always zero
  */
 int main(int argc, char *argv[])
 {
-	int i;
 	int mul;
 
-	for (i = 1; i < argc; i++)
+	if (argc != 3)
 	{
-		if (argc != 3)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			mul = atoi(argv[1]) * atoi(argv[2]);
-			printf("%d\n", mul);
-		}
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
 	}
 	return (0);
 }
