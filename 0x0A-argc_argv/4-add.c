53b8081da90cf31d04cf4950d1855c35412fca9f
Hikmat <hikmatyusuf2018@gmail.com>
2023-06-12 18:31:57 -0700

    sum of positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,20 +14,17 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) != 0 || atoi(argv[i]) != 9)
-		{
-			printf("Error\n");
-			return (1);
-		}
+		sum += atoi(argv[i]);
+		printf("%d\n", sum);
 	}
-	sum += atoi(argv[i]);
+	if (atoi(argv[i]) != 0 || atoi(argv[i]) != 9)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	if (argc == 1)
 	{
 		printf("0\n");
-	}
-	else
-	{
-		printf("%d\n", sum);
 	}
 	return (0);
 }
