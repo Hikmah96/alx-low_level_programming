83d244fad02fb18c9ed34775f986e001466e25d6
Hikmat <hikmatyusuf2018@gmail.com>
2023-06-14 13:04:31 -0700

    positive number

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - the main function
@@ -11,20 +12,22 @@ int main(int argc, char *argv[])
 {
 	int sum = 0;
 	int i;
+	int j;
 
+	if (argc < 2)
+		printf("0\n");
 	for (i = 1; i < argc; i++)
 	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		sum += atoi(argv[i]);
-		printf("%d\n", sum);
 	}
-	if (atoi(argv[i]) != 0 && atoi(argv[i]) != 9)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
+	printf("%d\n", sum);
 	return (0);
 }
