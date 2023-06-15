5c203fe3d6d7076e41e468e6c82270afa8d7e7a8
Hikmat <hikmatyusuf2018@gmail.com>
2023-06-15 16:39:08 -0700

    strncat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int h;
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
 	for (i = 0; s1[i] != '\0'; i++)
 		len1++;
 	for (i = 0; s2[i] != '\0'; i++)
