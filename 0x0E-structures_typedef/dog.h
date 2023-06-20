847e1366443a6741066c9842f50bf9d64cf56fe6
Hikmat <hikmatyusuf2018@gmail.com>
2023-06-20 14:58:14 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - name of struct
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
