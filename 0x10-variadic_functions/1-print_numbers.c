35ed497d0dd7c1e71cf17eba364cc8a341bd4e81
Hikmat <hikmatyusuf2018@gmail.com>
2023-06-22 17:21:05 -0700

    pn

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,13 +14,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(pnumbers, n);
 
-	if (separator == NULL)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(pnumbers, int);
 		printf("%d", x);
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 		printf("%s", separator);
 	}
 	printf("\n");
